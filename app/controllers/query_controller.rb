e0d4031eb86ea52188726b7186a79cd649ffbc1d
Angel Pizarro <angel@upenn.edu>
2010-09-15 20:15:37 +0000

    updated query

--- app/controllers/query_controller.rb
+++ app/controllers/query_controller.rb
@@ -1,7 +1,7 @@
 class QueryController < ApplicationController
   @@per_page = 25
   def index
-    params[:cosopt_q_value] ||=1.0
+    params[:cosopt_q_value] = params[:cosopt_q_value] ? params[:cosopt_q_value].to_f : 2.0
     cnd = {:cosopt_q_value => (0.0)..(params[:cosopt_q_value].to_f)}
     page = params[:page].to_i > 0 ? params[:page].to_i : 1
     qs = params[:query_string].to_s
@@ -12,7 +12,7 @@ class QueryController < ApplicationController
     if  (params[:assay].to_i > 0)
       cnd[:assay_id] = params[:assay].to_i
     end
-    @probeset_stats = ProbesetStat.search(qs, :with => cnd, :page => page, :per_page => @@per_page, :include => [:probeset], :order => "fisherg_p_value ASC")
+    @probeset_stats = ProbesetStat.search(qs, :with => cnd, :page => page, :per_page => @@per_page, :include => [:probeset, :probeset_data], :order => "cosopt_q_value ASC", :match_mode => :any)
     respond_to do |format|
       format.html 
       format.bgps { render :action => "index" , :layout => "biogps" }
