5dd61ca3c4e83354d931b42dde30916be160cea0
Angel Pizarro <angel@upenn.edu>
2010-09-16 13:25:24 +0000

    modified sphinx to specific port and index file location. modified query controller to ignore q_value filter if none specified

--- app/controllers/query_controller.rb
+++ app/controllers/query_controller.rb
@@ -1,18 +1,22 @@
 class QueryController < ApplicationController
   @@per_page = 25
   def index
-    params[:cosopt_q_value] = params[:cosopt_q_value] ? params[:cosopt_q_value].to_f : 2.0
-    cnd = {:cosopt_q_value => (0.0)..(params[:cosopt_q_value].to_f)}
+    cnd = {}
     page = params[:page].to_i > 0 ? params[:page].to_i : 1
     qs = params[:query_string].to_s
-    unless params[:advanced_query]
-      qs = qs.split("\s+").join(" | ")
+
+    # q_value filter
+    if params[:cosopt_q_value].to_f > 0.0
+      cnd[:cosopt_q_value] = (0.0)..(params[:cosopt_q_value].to_f)
     end
+
     # tissue
     if  (params[:assay].to_i > 0)
       cnd[:assay_id] = params[:assay].to_i
     end
-    @probeset_stats = ProbesetStat.search(qs, :with => cnd, :page => page, :per_page => @@per_page, :include => [:probeset, :probeset_data], :order => "cosopt_q_value ASC", :match_mode => :any)
+
+    @probeset_stats = ProbesetStat.search(qs, :with => cnd, :page => page, :per_page => @@per_page, :include => [:probeset, :probeset_data], :order => "jtk_q_value ASC", :match_mode => :any)
+
     respond_to do |format|
       format.html 
       format.bgps { render :action => "index" , :layout => "biogps" }
