fbe0a361d94954eeb3426629172c422294d15986
Katharina Hayer <katharinaehayer@gmail.com>
2012-09-18 16:41:34 -0400

    Routing correctly

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ ActionController::Routing::Routes.draw do |map|
   map.query '/query.:format', :controller => "query", :action => "index"
   map.help '/help', :controller => "query", :action => "help", :format => 'html'
   map.about '/about', :controller => "query", :action => "about", :format => 'html'
-  map.advanced_query_help '/advanced_query_help', :controller => "query", :action => "advanced_query_help", :format => 'html'
+  map.advanced_query_help "/help", :anchor => 'querying', :controller => "query", :action => "help", :format => 'html'
+  map.select_more_than_one "/help", :anchor => 'faq', :controller => "query", :action => "help", :format => 'html'
+  map.results "/help", :anchor => 'results', :controller => "query", :action => "help", :format => 'html'
 end
