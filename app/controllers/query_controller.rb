3e74e40ec80813ce99c64dea1f68aede1b0a7403
Katharina Hayer <katharinaehayer@gmail.com>
2014-03-10 18:25:50 -0400

    hitting the pause button

--- app/controllers/query_controller.rb
+++ app/controllers/query_controller.rb
@@ -1,16 +1,17 @@
 class QueryController < ApplicationController
   @per_page = 10
+  @k ||= nil
 
-  def download(filename)
-    send_data filename, :filename => 'query.csv', :type => 'text/csv'
+  def download
+    send_data @k, :filename => 'query.csv', :type => 'text/csv'
   end
 
   def index
-
     # condition hash
     cnd = {}
     # page to fetch
     current_page = params[:page].to_i > 0 ? params[:page].to_i : 1
+
 
     # q_value filter
     params[:filter] ||= "jtk_p_value"
@@ -54,8 +55,6 @@ class QueryController < ApplicationController
       end
     end
 
-
-
     if params[:query_string]
       @probeset_stats = ProbesetStat.search(params[:query_string],
         :page => current_page, :per_page => @per_page, :with => cnd,
@@ -68,64 +67,73 @@ class QueryController < ApplicationController
         :include => [:probeset_data, :probeset, :probeset_stats])
     end
 
-
-    if params[:query_string] && params[:match_mode] == 'gene_symbol'
-      fields = params[:query_string].split("@gene_symbol")
-      params[:query_string] = fields.join("\n").delete(" | ")
-    end
-
-    # if you want to log messages, look at the Rails logger functionality
-    # puts "@probeset_stats = #{@probeset_stats.length}"
-    respond_to do |format|
-      format.html
-      format.bgps do
-        @unigene_id = params[:query_string]
-        render :action => "index", :layout => "biogps"
-      end
-      format.js { render :json => @probeset_stats.to_json }
-      format.xml { render :xml => @probeset_stats.to_xml }
-    end
-
     if params[:number_entries].to_i > 0
       #filename = "#{RAILS_ROOT}/test.nina.txt"
       #logger.debug "Person attributes hash: #{@person.attributes.inspect}"
      #logger.info "Filename: #{filename}"
       #logger.fatal "Terminating application, raised unrecoverable error!!!"
       #puts filename
-      if params[:query_string]
-        probeset_stats = ProbesetStat.search(params[:query_string],
-          :page => current_page, :per_page => params[:number_entries].to_i,
-          :with => cnd,
-          :order => order, :match_mode => @match_mode,
-          :include => [:probeset_data, :probeset, :probeset_stats])
-      else
-        probeset_stats = ProbesetStat.search(:page => current_page, :per_page => params[:number_entries].to_i,
-          :with => cnd,
-          :order => order,
-          :include => [:probeset_data, :probeset, :probeset_stats])
-      end
-      k = "Probeset_ID,Symbol,Time,Values,JTKP,JTKQ,JTKperiod,JTKphase\n"
-      for i in 0...[params[:number_entries].to_i,probeset_stats.length].min
-        probeset_stat = probeset_stats[i]
-        probeset = probeset_stats[i].probeset
+      #if params[:query_string]
+      #  xprobeset_stats = ProbesetStat.search(params[:query_string],
+      #    :page => current_page, :per_page => params[:number_entries].to_i,
+      #    :with => cnd,
+      #    :order => order, :match_mode => @match_mode,
+      #    :include => [:probeset_data, :probeset, :probeset_stats])
+      #else
+      #  xprobeset_stats = ProbesetStat.search(:page => current_page, :per_page => params[:number_entries].to_i,
+      #    :with => cnd,
+      #    :order => order,
+      #    :include => [:probeset_data, :probeset, :probeset_stats])
+      #end
+      @k = "Probeset_ID,Symbol,Time,Values,JTKP,JTKQ,JTKperiod,JTKphase\n"
+      for i in 0...params[:number_entries].to_i
+        probeset_stat = @probeset_stats[i]
+        break unless probeset_stat
+        probeset = @probeset_stats[i].probeset
         next unless probeset
         gene_symbol = probeset.gene_symbol
         id = probeset.probeset_name
         probeset_data = probeset_stat.probeset_data
         time_points = probeset_data.time_points.delete("\"[]").gsub(/,/,';')
         data_points = probeset_data.data_points.delete("\"[]").split(",")
-        data_points = data_points.map { |e| (e.to_f*100).round/100.to_f} 
+        data_points = data_points.map { |e| (e.to_f*100).round/100.to_f}
         data_points = data_points.join(";")
         jtkp = probeset_stat.jtk_p_value
         jtkq = probeset_stat.jtk_q_value
         jtkperiod = probeset_stat.jtk_period_length
         jtkphase = probeset_stat.jtk_lag
-        k += "#{id},#{gene_symbol},#{time_points},#{data_points},#{jtkp},#{jtkq},#{jtkperiod},#{jtkphase}\n"
+        @k += "#{id},#{gene_symbol},#{time_points},#{data_points},#{jtkp},#{jtkq},#{jtkperiod},#{jtkphase}\n"
       end
-      
-      download(k)
+
+
       #File.delete(filename)
+    else
+      @k = nil
     end
+
+    if params[:query_string] && params[:match_mode] == 'gene_symbol'
+      fields = params[:query_string].split("@gene_symbol")
+      params[:query_string] = fields.join("\n").delete(" | ")
+    end
+
+    #download(k) if k
+
+
+    # if you want to log messages, look at the Rails logger functionality
+    # puts "@probeset_stats = #{@probeset_stats.length}"
+    respond_to do |format|
+
+      format.html
+      format.bgps do
+        @unigene_id = params[:query_string]
+        render :action => "index", :layout => "biogps"
+      end
+      format.js { render  :json => @probeset_stats.to_json }
+      format.xml { render  :xml => @probeset_stats.to_xml }
+    end
+
+    send_data @k, :filename => 'query.csv', :type => 'text/csv' if @k
   end
+
 end
 
--- app/controllers/table_controller.rb
+++ app/controllers/table_controller.rb
@@ -1,99 +1,111 @@
 class TableController < ApplicationController
   helper :all # include all helpers, all the time
   protect_from_forgery
-  @@per_page = 50
+
+  def none
+
+  end
 
   def write
-    # condition hash
-    cnd = {}
-    # page to fetch
-    current_page = params[:page].to_i > 0 ? params[:page].to_i : 1
+    if params[:number_entries].to_i > 0
+      # condition hash
+      cnd = {}
+      # page to fetch
+      #current_page = params[:page].to_i > 0 ? params[:page].to_i : 1
 
-    # q_value filter
-    params[:filter] ||= "jtk_p_value"
-    if ProbesetStat.pval_filters.flatten.include?(params[:filter])
-      order = "#{params[:filter]} ASC"
-    else
-      order = "jtk_p_value ASC"
-    end
-    fv = params[:filter_value].to_f > 0.0 ? params[:filter_value].to_f : 0.05
-    cnd[params[:filter].to_sym] = (0.0)..(fv)
+      # q_value filter
+      params[:filter] ||= "jtk_p_value"
+      if ProbesetStat.pval_filters.flatten.include?(params[:filter])
+        order = "#{params[:filter]} ASC"
+      else
+        order = "jtk_p_value ASC"
+      end
+      fv = params[:filter_value].to_f > 0.0 ? params[:filter_value].to_f : 0.05
+      cnd[params[:filter].to_sym] = (0.0)..(fv)
 
-    # tissue
-    cnd[:assay_id] = params[:assays] if params[:assays]
-    if params[:query_string].to_s.strip.empty?
-      params[:query_string] = nil
-    end
+      # tissue
+      cnd[:assay_id] = params[:assays] if params[:assays]
+      if params[:query_string].to_s.strip.empty?
+        params[:query_string] = nil
+      end
 
-    # phase
-    params[:phase_range] = "0-40" if params[:phase_range] == nil #? params[:phase_range] : "0-40"
-    pv = params[:phase_range].split("-")
-    pv_lower = pv[0].to_f
-    pv_upper = pv[1].to_f
-    cnd[:jtk_lag] = (pv_lower)..(pv_upper)
+      # phase
+      params[:phase_range] = "0-40" if params[:phase_range] == nil #? params[:phase_range] : "0-40"
+      pv = params[:phase_range].split("-")
+      pv_lower = pv[0].to_f
+      pv_upper = pv[1].to_f
+      cnd[:jtk_lag] = (pv_lower)..(pv_upper)
 
 
 
-    # query match mode
-    params[:match_mode] ||= 'gene_symbol'
-    @match_mode = params[:match_mode].to_sym
+      # query match mode
+      params[:match_mode] ||= 'gene_symbol'
+      @match_mode = params[:match_mode].to_sym
 
-    if params[:match_mode] == 'gene_symbol' && params[:query_string]
-      @new_query = ""
-      if params[:query_string]
-        @match_mode = "extended".to_sym
-        params[:query_string].split(" ").each do |word|
-          @new_query += "@gene_symbol #{word} | "
+      if params[:match_mode] == 'gene_symbol' && params[:query_string]
+        @new_query = ""
+        if params[:query_string]
+          @match_mode = "extended".to_sym
+          params[:query_string].split(" ").each do |word|
+            @new_query += "@gene_symbol #{word} | "
+          end
+          params[:query_string] = @new_query[0..-3]
         end
-        params[:query_string] = @new_query[0..-3]
       end
-    end
 
-    if params[:query_string]
-      @probeset_stats = ProbesetStat.search(params[:query_string],
-        :page => current_page, :per_page => params[:number_entries].to_i,
-        :with => cnd,
-        :order => order, :match_mode => @match_mode,
-        :include => [:probeset_data, :probeset, :probeset_stats])
-    else
-      @probeset_stats = ProbesetStat.search(:page => current_page, :per_page => params[:number_entries].to_i,
-        :with => cnd,
-        :order => order,
-        :include => [:probeset_data, :probeset, :probeset_stats])
-    end
-
-
-    if params[:query_string] && params[:match_mode] == 'gene_symbol'
-      fields = params[:query_string].split("@gene_symbol")
-      params[:query_string] = fields.join("\n").delete(" | ")
-    end
-
-    # if you want to log messages, look at the Rails logger functionality
-    # puts "@probeset_stats = #{@probeset_stats.length}"
-    @text = "ID\tSymbol\tTime\tValues\tJTKP\tJTKQ\tJTKperiod\tJTKphase\n"
-    @probeset_stats.each do |probeset_stat|
-      probeset = probeset_stat.probeset
-      next unless probeset
-      gene_symbol = probeset.gene_symbol
-      id = probeset.probeset_name
-      probeset_data = probeset_stat.probeset_data
-      time_points = probeset_data.time_points.delete("\"[]")
-      data_points = probeset_data.data_points.delete("\"[]")
-      jtkp = probeset_stat.jtk_p_value
-      jtkq = probeset_stat.jtk_q_value
-      jtkperiod = probeset_stat.jtk_period_length
-      jtkphase = probeset_stat.jtk_lag
-      @text = @text + "#{id}\t#{gene_symbol}\t#{time_points}\t#{data_points}\t#{jtkp}\t#{jtkq}\t#{jtkperiod}\t#{jtkphase}\n"
-    end
-
-    respond_to do |format|
-      format.html
-      format.bgps do
-        @unigene_id = params[:query_string]
-        render :action => "write", :layout => "biogps"
+      if params[:query_string]
+        @probeset_stats = ProbesetStat.search(params[:query_string],
+          #:page => current_page, :per_page => params[:number_entries].to_i,
+          :with => cnd, :limit => params[:number_entries].to_i,
+          :order => order, :match_mode => @match_mode,
+          :include => [:probeset_data, :probeset, :probeset_stats])
+      else
+        @probeset_stats = ProbesetStat.search(#:page => current_page, :per_page => params[:number_entries].to_i,
+          :with => cnd, :limit => params[:number_entries].to_i,
+          :order => order,
+          :include => [:probeset_data, :probeset, :probeset_stats])
       end
-      format.js { render :json => @probeset_stats.to_json }
-      format.xml { render :xml => @probeset_stats.to_xml }
+
+
+      if params[:query_string] && params[:match_mode] == 'gene_symbol'
+        fields = params[:query_string].split("@gene_symbol")
+        params[:query_string] = fields.join("\n").delete(" | ")
+      end
+
+      # if you want to log messages, look at the Rails logger functionality
+      # puts "@probeset_stats = #{@probeset_stats.length}"
+      if params[:number_entries].to_i > 0
+        @k = "Probeset_ID,Symbol,Time,Values,JTKP,JTKQ,JTKperiod,JTKphase\n"
+        for i in 0...@probeset_stats.length
+          probeset_stat = @probeset_stats[i]
+          probeset = @probeset_stats[i].probeset
+          next unless probeset
+          gene_symbol = probeset.gene_symbol
+          id = probeset.probeset_name
+          probeset_data = probeset_stat.probeset_data
+          time_points = probeset_data.time_points.delete("\"[]").gsub(/,/,';')
+          data_points = probeset_data.data_points.delete("\"[]").split(",")
+          data_points = data_points.map { |e| (e.to_f*100).round/100.to_f}
+          data_points = data_points.join(";")
+          jtkp = probeset_stat.jtk_p_value
+          jtkq = probeset_stat.jtk_q_value
+          jtkperiod = probeset_stat.jtk_period_length
+          jtkphase = probeset_stat.jtk_lag
+          @k += "#{id},#{gene_symbol},#{time_points},#{data_points},#{jtkp},#{jtkq},#{jtkperiod},#{jtkphase}\n"
+        end
+      end
+
+      respond_to do |format|
+        format.html
+        format.bgps do
+          @unigene_id = params[:query_string]
+          render :action => "write", :layout => "biogps"
+        end
+        format.js { render :json => @probeset_stats.to_json }
+        format.xml { render :xml => @probeset_stats.to_xml }
+      end
+
+      send_data @k, :filename => 'query.csv', :type => 'text/csv' if @k
     end
   end
 end
