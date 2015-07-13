4b6aa48a31e43a89948c7dd233d4f8bfcb544820
Katharina Hayer <katharinaehayer@gmail.com>
2015-07-13 10:06:23 -0400

    hitting the pause button

--- config/application.rb
+++ config/application.rb
@@ -19,9 +19,6 @@ module TestRails
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.action_dispatch.default_headers = {
-      'X-Frame-Options' => ''
-    }
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
