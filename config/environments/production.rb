8a28d6d759a73bbaf6132e92ba456f16181a1a9f
Katharina Hayer <katharinaehayer@gmail.com>
2015-07-13 15:02:07 -0400

    hitting the pause button

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,12 +2,13 @@
 
 # The production environment is meant for finished, "live" apps.
 # Code is not reloaded between requests
-config.cache_classes = true
+Rails.application.configure do
+  config.cache_classes = true
 
-# Full error reports are disabled and caching is turned on
-config.action_controller.consider_all_requests_local = false
-config.action_controller.perform_caching             = true
-config.action_view.cache_template_loading            = true
+  # Full error reports are disabled and caching is turned on
+  config.action_controller.consider_all_requests_local = false
+  config.action_controller.perform_caching             = true
+  config.action_view.cache_template_loading            = true
 
 # See everything in the log (default is :info)
 # config.log_level = :debug
@@ -26,3 +27,4 @@ config.action_view.cache_template_loading            = true
 
 # Enable threaded mode
 # config.threadsafe!
+end
